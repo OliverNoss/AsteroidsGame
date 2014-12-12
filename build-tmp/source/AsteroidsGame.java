301684a919fa33011e74b07879765688865431d8
OliverNoss <odnoss@sbcglobal.net>
2014-12-12 11:02:23 -0800

    done

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -15,43 +15,88 @@ import java.io.IOException;
 public class AsteroidsGame extends PApplet {
 
 //Oliver Noss CompSci Block4
+//make a marked for death function in bullet class, remove all remaining listofDeath stuff
 SpaceShip voyager = new SpaceShip();
 Star [] galaxy = new Star[200];
-// Asteroid [] belt = new Asteroid[25];
 ArrayList <Asteroid> belt = new ArrayList <Asteroid>();
-//Work on collide at line 300 
+ArrayList <Bullet> magazine = new ArrayList <Bullet>();
+boolean gameover = false;
+boolean win;
+int asterstroyed;
+boolean godmode=false;
 public void setup() 
 {
+  asterstroyed=0;
+  gameover = false;
   size(700,700);
   background(0);
   stroke(255);
-  // frameRate(5);
+  
   for (int i = 0; i < galaxy.length; i++) {
     galaxy[i] = new Star();
   }
-  for (int i = 0; i < 10; i++) {
+  // for (int i = 0; i < 44; i++) {
+    while (belt.size()<80)
+    {
     belt.add(new Asteroid());
   }
 }
 public void draw() 
 {
-  background(0);
-  for (int i = 0; i < galaxy.length; i++) {
-    galaxy[i].show();
-  }
-  for (int i = 0; i < belt.size(); i++) {
-    belt.get(i).show();
-    belt.get(i).move();
-  }
-  collide();
-  strokeWeight(1);
-  voyager.rockets();
-  voyager.show();
-  voyager.move();
-  // james.show();
+  if (!gameover)
+  {// System.out.println(magazine.size());
+    background(0);
+    for (int i = 0; i < galaxy.length; i++) {
+      galaxy[i].show();
+    }
+    for (int i = 0; i < belt.size(); i++) {
+      belt.get(i).show();
+      belt.get(i).move();
+    }
+    strokeWeight(1);
+    voyager.rockets();
+    voyager.show();
+    voyager.move();
+    for (int i = 0; i < magazine.size(); i++) { 
+      magazine.get(i).show();
+      magazine.get(i).move();
+      if (magazine.get(i).bulletEdge() || magazine.get(i).getDeath())
+        magazine.remove(i);
+    } 
+    collide();
+
   // fade();
 }
-
+else {
+  if (win)
+    {
+      stroke(0,255,0);
+      fill(0,255,0);
+      textSize(20);
+      textAlign(CENTER);
+      text("You somehow managed to win, cheater", 370,200);
+      
+    }
+  else if(!win)
+  {
+    
+    stroke(255,0,0);
+      fill(255,0,0);
+      textSize(20);
+      textAlign(CENTER);
+      text("You destroyed "+ asterstroyed + " asteroids before your inevitable demise.",370,200);
+  }
+}
+}
+public void mousePressed()
+{
+  if (gameover)
+  {
+    setup();
+    gameover=false;
+    voyager.setLives(5);
+  }
+}
 public void keyPressed()
 {
   if (key == 'a')
@@ -72,6 +117,10 @@ public void keyPressed()
   }
   if (key == 'h')
     voyager.hyperspace();
+  if (key== ' ')
+    magazine.add(new Bullet(voyager));
+  if(godmode)
+  {
   if (key == CODED)
   {
     if (keyCode == UP)
@@ -80,8 +129,14 @@ public void keyPressed()
       belt.remove(belt.size()-1);
   }
 }
+  if(key=='O')
+  {
+    godmode=!godmode;
+  }
+}
 class SpaceShip extends Floater  
 {
+  private int lives;
   public SpaceShip()
   {
     corners=5;
@@ -103,7 +158,7 @@ class SpaceShip extends Floater
     myDirectionX = 0;
     myDirectionY = 0;
     myPointDirection = 0;
-
+    lives=5;
   }
   public void hyperspace()
   {
@@ -139,6 +194,8 @@ class SpaceShip extends Floater
  public double getDirectionY() {return myDirectionY;}   
  public void setPointDirection(int degrees) {myPointDirection=degrees;}   
  public double getPointDirection() {return myPointDirection;} 
+ public void setLives(int x) {lives=x;}  
+ public int getLives() {return lives;}
 }//
 abstract class Floater //Do NOT modify the Floater class! Make changes in the SpaceShip class 
 {   
@@ -288,7 +345,7 @@ class Asteroid extends Floater
       xCorners=x2;
       yCorners=y2;
     }
-     else if(type==3)
+    else if(type==3)
     {
       corners=11;
       xCorners=x3;
@@ -313,9 +370,10 @@ class Asteroid extends Floater
   }
   public void destroy()
   {
-    System.out.println("Boom");
+    // System.out.println("Boom");
     fill(225,80,0,20);
     fade((float)myCenterX,(float)myCenterY,mySize*11);
+    asterstroyed++;
     // ellipse((float)myCenterX,(float)myCenterY,mySize*11,mySize*11);
   }
 
@@ -323,15 +381,118 @@ class Asteroid extends Floater
 // 
 public void collide()
 {
-  for(int i = 0; i< belt.size(); i++)//turn into normal for loop so remove works
+  for (int i = 0; i < belt.size(); i++)//turn into normal for loop so remove works
   {
     if (dist(belt.get(i).getX(),belt.get(i).getY(),voyager.getX(),voyager.getY()) < belt.get(i).getSize()*3+10)
     {
       belt.get(i).destroy();
-      belt.remove(i);// write destroy function
+      belt.remove(i);
+      if (!godmode)
+      voyager.setLives(voyager.getLives()-1);
+      if (voyager.getLives()==0)
+      {
+        gameover=true;
+        win=false;
+      }
+      else if (voyager.getLives()>0 && belt.size()==0)
+      {
+       gameover=true;
+       win=true;
+     }
+   }
+ }
+  for (int j = 0; j < magazine.size(); j++)//turn into normal for loop so remove works
+  {
+    for (int k = 0; k < belt.size(); k++)
+    {
+      // System.out.println("2nd for");
+      if (dist(belt.get(k).getX(),belt.get(k).getY(),magazine.get(j).getX(),magazine.get(j).getY()) < belt.get(k).getSize()*3+2)
+      {
+        belt.get(k).destroy();
+        magazine.get(j).setDeath(true);
+        belt.remove(k);
+        if (voyager.getLives()>0 && belt.size()==0)
+      {
+       gameover=true;
+       win=true;
+     }
+      }
+
     }
+
   }
 }
+class Bullet extends Floater
+{
+  private boolean isDead;
+  public Bullet(SpaceShip shipName)
+  {
+    myCenterX=shipName.getX();
+    myCenterY=shipName.getY();
+    myPointDirection=shipName.getPointDirection();
+    double dRadians = myPointDirection*(Math.PI/180);
+    myDirectionX = -5*Math.cos(dRadians)+shipName.getDirectionX();
+    myDirectionY = -5*Math.sin(dRadians)+shipName.getDirectionY();
+    isDead=false;
+  }
+  public void show()
+  {
+      // stroke(0,255,0);
+      fill(0,250,0);
+      ellipse((float)myCenterX,(float)myCenterY,5,5);
+    }
+  //   public void move()
+  //   {
+  //     myCenterX += myDirectionX;    
+  //     myCenterY += myDirectionY;     
+
+  //   //wrap around screen    
+  //   if(myCenterX > width+5)
+  //   {     
+  //     myCenterX = 0;    
+  //   }    
+  //   else if (myCenterX < 0-5)
+  //   {     
+  //     myCenterX = width;    
+  //   }    
+  //   if(myCenterY > height+5)
+  //   {    
+  //     myCenterY = 0;    
+  //   }   
+  //   else if (myCenterY < 0-5)
+  //   {     
+  //     myCenterY = height;    
+  //   } 
+  // }  
+  public void setX(int x) {myCenterX=x;}  
+  public int getX() {return (int)myCenterX;}
+  public void setY(int y) {myCenterY=y;}   
+  public int getY() {return (int)myCenterY;}   
+  public void setDirectionX(double x) {myDirectionX=x;}   
+  public double getDirectionX() {return myDirectionX;}   
+  public void setDirectionY(double y) {myDirectionY=y;}   
+  public double getDirectionY() {return myDirectionY;}   
+  public void setPointDirection(int degrees) {myPointDirection=degrees;}   
+  public double getPointDirection() {return myPointDirection;} 
+  public void setDeath(boolean bTemp) {isDead=bTemp;}
+  public boolean getDeath() {return isDead;}
+  public void move()
+  {
+    myCenterX += myDirectionX;    
+    myCenterY += myDirectionY;  
+  }
+  
+  public boolean bulletEdge()
+  {
+  // boolean temp = false;
+  // boolean secondcrossing = false;  
+  if (myCenterX>width||myCenterX<0||myCenterY>height||myCenterY<0)
+   return true;
+ else 
+  return false;
+
+}
+}//stop the bullets
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "AsteroidsGame" };
     if (passedArgs != null) {
